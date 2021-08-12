6a6753f9cfa5de423357419eb162bbecbbcc6075
sh_chixx <chixx@dwzq.com.cn>
2021-08-12 09:47:46 +0800

    fix: start error

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'cross.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [new CleanWebpackPlugin(),
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "cross.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  stats: {
+    children: false,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-        inject: 'head',
-        template: 'index.html'
-    })]
-}
+      inject: "head",
+      template: "index.html",
+    }),
+  ],
+};
